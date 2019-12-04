bc762b3a7b092954650defd455a48a779cfacde9
xuanbach1992 <Xuanbach1992>
2019-12-04 13:35:10 +0700

    drop down edit profile

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('/user')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::post('/edit', 'HomeController@editShow')->name('user.edit');
+});
