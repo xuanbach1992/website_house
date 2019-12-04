daf0c93cf0d33d0dc8f452deb0cceaa3057212e5
TranManhHiep <hieptm.it@gmail.com>
2019-12-04 15:34:18 +0700

    tạo form cho create house

--- resources/views/house/add.blade.php
+++ resources/views/house/add.blade.php
@@ -1,5 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="col-lg-8">
+        <form method="post" action="">
+            @csrf
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Tên chủ nhà : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập tên chủ nhà">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Loại nhà : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập loại nhà">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Loại phòng : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập loại phòng">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Địa chỉ : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập địa chỉ">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Số lượng phòng ngủ : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập số phòng ngủ">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Số lượng phòng tắm : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập số phòng tắm">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Ảnh phòng : </h5></label>
+                <input type="file" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1"><h5>Mô tả : </h5></label>
+                <textarea class="form-control"  rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1"><h5>Giá tiền theo đêm : </h5></label>
+                <input type="text" class="form-control" placeholder="Nhập giá phòng">
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary">Create</button>
+            </div>
+        </form>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,13 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
         </main>
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
