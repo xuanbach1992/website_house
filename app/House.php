fa23fc93d716411f3899a44b0febadb107b0329b
TranManhHiep <hieptm.it@gmail.com>
2019-12-04 15:03:34 +0700

    thêm trường dữ liệu trong model house

--- app/House.php
+++ app/House.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    //
+    protected $table = 'houses';
+
+    protected $fillable = [
+        'id', 'name', 'address', 'house_type' ,'room_type' ,
+        'bedrooms' ,'bathroom' ,'description' ,'price',
+    ];
 }
