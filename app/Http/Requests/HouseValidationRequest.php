46ad6f69553a7177bb4735ff67e3124f89a44a75
TranManhHiep <hieptm.it@gmail.com>
2019-12-05 10:51:40 +0700

    create file HouseVadidate

---
+++ app/Http/Requests/HouseValidationRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HouseValidationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'address' => 'required',
+            'house_type' => 'required',
+            'room_type' => 'required',
+            'bedrooms' => 'required',
+            'bathroom' => 'required',
+            'price' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Tên không được bỏ trống',
+            'address' => 'Địa chỉ không được bỏ trống',
+            'house_type' => 'Kiểu nhà không được bỏ trống',
+            'room_type' => 'Kiểu phòng không được bỏ trống',
+            'bedrooms' => 'Số phòng ngủ không được bỏ trống',
+            'bathroom' => 'Số phòng tắm không được bỏ trống',
+            'price' => 'Giá tiền không được bỏ trống',
+        ];
+    }
+}
