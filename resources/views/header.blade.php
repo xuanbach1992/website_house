8a79708c6f36c37acf1350af28d6557ea17dd577
xuanbach1992 <Xuanbach1992>
2019-12-08 13:58:22 +0700

    include template

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,63 +1,194 @@
 <!--header-->
-<header class="site-navbar site-navbar-target" role="banner">
+<div id="app">
+    <div class="modal fade" id="exampleModalLong_2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+         aria-hidden="true">
 
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalScrollableTitle">Log in</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email"
+                                   class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password"
+                                   class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password"
+                                       class="form-control @error('password') is-invalid @enderror" name="password"
+                                       required autocomplete="current-password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="exampleModalLong_1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalScrollableTitle">Change password</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="form-change-password" role="form" method="POST" action="{{ route('user.editPass') }}"
+                          novalidate class="form-horizontal">
+                        @csrf
+                        <div>
+                            <label class="control-label">Current Password</label>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="current-password"
+                                       name="current-password" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <label>New Password</label>
+                                <input type="password" class="form-control" id="password" name="password"
+                                       placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label">Re-enter Password</label>
+                                <input type="password" class="form-control" id="password_confirmation"
+                                       name="password_confirmation" placeholder="Re-enter Password">
+                            </div>
+                        </div>
+                        <div class="modal-footer form-group">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<header class="site-navbar site-navbar-target" role="banner">
     <div class="container">
         <div class="row align-items-center position-relative">
-
             <div class="col-3 ">
                 <div class="site-logo">
                     <a href="{{route('index')}}">Realtors</a>
                 </div>
             </div>
-
             <div class="col-9  text-right">
 
-
                 <span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
-
-
-
                 <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
                     <ul class="site-menu main-menu js-clone-nav ml-auto ">
-                        <li class="active"><a href="{{route('index')}}" class="nav-link">Trang chủ</a></li>
-                        {{--<li><a href="agents.html" class="nav-link">Agents</a></li>--}}
+                        <li class="active"><a href="/" class="nav-link">Trang chủ</a></li>
                         <li><a href="{{route('product')}}" class="nav-link">Sản phẩm</a></li>
                         <li><a href="{{route('about')}}" class="nav-link">Giới thiệu</a></li>
                         <li><a href="{{route('blog')}}" class="nav-link">Blog</a></li>
                         <li><a href="{{route('contact')}}" class="nav-link">Liên Hệ</a></li>
+
+                                <li class="nav-item">
+                                    @guest
+                                    <a class="nav-link" href="{{ route('login') }}" data-toggle="modal"
+                                       data-target="#exampleModalLong_2">
+                                        {{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ \Illuminate\Support\Facades\Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <a class="dropdown-item"
+                                           href="{{ route('user.edit',\Illuminate\Support\Facades\Auth::user()->id)}}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('edit_user').submit();">
+                                            {{ __('Edit Profile') }}
+                                        </a>
+
+                                        <a class="dropdown-item"
+                                           data-toggle="modal" data-target="#exampleModalLong_1">
+                                            Change Password
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
+                                            @csrf
+                                        </form>
+                                        <form id="edit_user"
+                                              action="{{ route('user.edit',\Illuminate\Support\Facades\Auth::user()->id)}}"
+                                              method="POST"
+                                              style="display: none;">
+                                            @csrf
+                                        </form>
+
+
+                                    </div>
+                                </li>
+                            @endguest
                     </ul>
+
                 </nav>
             </div>
 
-
         </div>
     </div>
-
 </header>
 <!--header-->
-<!--slice-->
-<div class="ftco-blocks-cover-1">
-    <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('{{asset('source/images/hero_1.jpg')}}')">
-        <div class="container">
-            <div class="row align-items-center justify-content-center text-center">
-                <!--thông tin-->
-                <div class="col-md-7">
-                    {{--<span class="h4 text-primary mb-4 d-block">$1,570,000</span>--}}
-                    <h1 class="mb-2">Beautiful House</h1>
-                    <p class="text-center mb-5"><span class="small address d-flex align-items-center justify-content-center"> <span class="icon-room mr-3 text-primary"></span> <span>Hà Nội - Việt Nam</span></span></p>
 
-                    <div class="d-flex media-38289 justify-content-around mb-5">
-                        {{--<div class="sq d-flex align-items-center"><span class="wrap-icon icon-fullscreen">--}}
-                                {{--</span> <span>2911 Sq Ft.</span></div>--}}
-                        {{--<div class="bed d-flex align-items-center"><span class="wrap-icon icon-bed"></span> <span>2</span></div>--}}
-                        {{--<div class="bath d-flex align-items-center"><span class="wrap-icon icon-bath"></span> <span>2</span></div>--}}
-                        <div class="bed d-flex align-items-center"><span>BTH</span></div>
-                    </div>
-                    <p><a href="{{route('about')}}" class="btn btn-primary text-white px-4 py-3">Learn More</a></p>
-                </div>
-                <!---->
-            </div>
-        </div>
-    </div>
-</div>
-<!--slice-->
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,215 +1,60 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
+    <title>Realtors &mdash; Website Template by Colorlib</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="{{asset('')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{asset('source/fonts/icomoon/style.css')}}">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{asset('source/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('source/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('source/css/jquery.fancybox.min.css')}}">
+    <link rel="stylesheet" href="{{asset('source/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('source/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('source/fonts/flaticon/font/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('source/css/aos.css')}}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="{{asset('source/css/style.css')}}">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-<body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                Home
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}" data-toggle="modal"
-                               data-target="#exampleModalLong_2">
-                                {{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ \Illuminate\Support\Facades\Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <a class="dropdown-item"
-                                   href="{{ route('user.edit',\Illuminate\Support\Facades\Auth::user()->id)}}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('edit_user').submit();">
-                                    {{ __('Edit Profile') }}
-                                </a>
-
-                                <a class="dropdown-item"
-                                   data-toggle="modal" data-target="#exampleModalLong_1">
-                                    Change Password
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                                <form id="edit_user"
-                                      action="{{ route('user.edit',\Illuminate\Support\Facades\Auth::user()->id)}}"
-                                      method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 
 
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+<div class="site-wrap" id="home-section">
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
             </div>
         </div>
-    </nav>
-
-    <div class="modal fade" id="exampleModalLong_2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
-         aria-hidden="true">
-
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalScrollableTitle">Log in</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
-                <div class="modal-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email"
-                                   class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password"
-                                   class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                       class="form-control @error('password') is-invalid @enderror" name="password"
-                                       required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <div class="site-mobile-menu-body"></div>
     </div>
 
 
-    <div class="modal fade" id="exampleModalLong_1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalScrollableTitle">Change password</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="form-change-password" role="form" method="POST" action="{{ route('user.editPass') }}"
-                          novalidate class="form-horizontal">
-                        @csrf
-                        <div>
-                            <label class="control-label">Current Password</label>
-                            <div class="form-group">
-                                <input type="password" class="form-control" id="current-password"
-                                       name="current-password" placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <label>New Password</label>
-                                <input type="password" class="form-control" id="password" name="password"
-                                       placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label">Re-enter Password</label>
-                                <input type="password" class="form-control" id="password_confirmation"
-                                       name="password_confirmation" placeholder="Re-enter Password">
-                            </div>
-                        </div>
-                        <div class="modal-footer form-group">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!--header-->
+{{--header ở đây--}}
+<!--header-->
+@include('header')
+<!--slice-->
+{{--@cannot('register')--}}
+    @include('slice')
+{{--@endcan--}}
+
+<!--slice-->
+
+
+
+    <!--homepage content-->
+
+{{--có 5 thẻ ở đây để hiển thị nội dung . --}}
     <main class="py-4">
         <div class="container">
             <div class="row">
@@ -219,7 +64,33 @@
             </div>
         </div>
     </main>
+
+<!--homepage content-->
+
+
+    <!--footer-->
+@include('footer')
+<!--footer-->
+
 </div>
 
+<script src="{{asset('source/js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('source/js/jquery-migrate-3.0.0.js')}}"></script>
+<script src="{{asset('source/js/popper.min.js')}}"></script>
+<script src="{{asset('source/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('source/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('source/js/jquery.sticky.js')}}"></script>
+<script src="{{asset('source/js/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('source/js/jquery.animateNumber.min.js')}}"></script>
+<script src="{{asset('source/js/jquery.fancybox.min.js')}}"></script>
+<script src="{{asset('source/js/jquery.stellar.min.js')}}"></script>
+<script src="{{asset('source/js/jquery.easing.1.3.js')}}"></script>
+<script src="{{asset('source/js/bootstrap-datepicker.min.js')}}"></script>
+<script src="{{asset('source/js/aos.js')}}"></script>
+
+<script src="{{asset('source/js/main.js')}}"></script>
+
 </body>
+
 </html>
+
