b4f2b4c7ab9e45bbbf85fe3fa953cb67aaf9d4d2
xuanbach1992 <Xuanbach1992>
2019-12-12 08:39:41 +0700

    refactor toast

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -95,7 +95,7 @@
                 <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
                     <ul class="site-menu main-menu js-clone-nav ml-auto ">
                         <li class="active"><a href="/" class="nav-link">Trang chủ</a></li>
-                        <li><a href="{{route('product')}}" class="nav-link" >Sản phẩm</a></li>
+                        <li><a href="{{route('index')}}" class="nav-link" >Sản phẩm</a></li>
                         <li><a href="{{route('about')}}" class="nav-link">Giới thiệu</a></li>
                         <li><a href="{{route('blog')}}" class="nav-link">Blog</a></li>
                         <li><a href="#"  id="product" class="nav-link">Liên Hệ</a></li>
