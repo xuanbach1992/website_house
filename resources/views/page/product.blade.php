b989cc1a67387e44531ea6398d670248b4c38f7e
TranManhHiep <hieptm.it@gmail.com>
2019-12-13 09:30:48 +0700

    sửa form tìm kiếm

--- resources/views/page/product.blade.php
+++ resources/views/page/product.blade.php
@@ -7,39 +7,22 @@
             <div class="container">
                 <div class="realestate-filter-wrap nav">
                     <a href="#for-rent" class="active" data-toggle="tab" id="rent-tab" aria-controls="rent"
-                       aria-selected="true">For Rent</a>
+                       aria-selected="true">Chưa cho thuê</a>
                     <a href="#for-sale" class="" data-toggle="tab" id="sale-tab" aria-controls="sale"
-                       aria-selected="false">For Sale</a>
+                       aria-selected="false">Đã cho thuê</a>
                 </div>
             </div>
         </div>
 
         <div class="realestate-tabpane pb-5 " id="demo">
             <div class="container tab-content">
+                <!--for-rent-->
                 <div class="tab-pane active" id="for-rent" role="tabpanel" aria-labelledby="rent-tab">
-
+                    <!--dòng 1-->
                     <div class="row">
                         <div class="col-md-4 form-group">
                             <select name="" id="" class="form-control w-100">
-                                <option value="">All Types</option>
-                                <option value="">Townhouses</option>
-                                <option value="">Duplexes</option>
-                                <option value="">Quadplexes</option>
-                                <option value="">Condominiums</option>
-                            </select>
-                        </div>
-                        <div class="col-md-4 form-group">
-                            <input type="text" class="form-control" placeholder="Title">
-                        </div>
-                        <div class="col-md-4 form-group">
-                            <input type="text" class="form-control" placeholder="Address">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-4 form-group">
-                            <select name="" id="" class="form-control w-100">
-                                <option value="">Any Bedrooms</option>
+                                <option value="">Any Bedrooms - phòng ngủ</option>
                                 <option value="">0</option>
                                 <option value="">1</option>
                                 <option value="">2</option>
@@ -47,8 +30,23 @@
                             </select>
                         </div>
                         <div class="col-md-4 form-group">
+                            <input type="text" class="form-control" placeholder="Địa chỉ">
+                        </div>
+                        <div class="col-md-4 form-group">
                             <select name="" id="" class="form-control w-100">
-                                <option value="">Any Bathrooms</option>
+                                <option value="">Thành phố</option>
+                                <option value="">0</option>
+                                <option value="">1</option>
+                                <option value="">2</option>
+                                <option value="">3</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!--dòng 2-->
+                    <div class="row">
+                        <div class="col-md-4 form-group">
+                            <select name="" id="" class="form-control w-100">
+                                <option value="">Any Bathrooms - phòng tắm</option>
                                 <option value="">0</option>
                                 <option value="">1</option>
                                 <option value="">2</option>
@@ -59,7 +57,7 @@
                             <div class="row">
                                 <div class="col-md-6 form-group">
                                     <select name="" id="" class="form-control w-100">
-                                        <option value="">Min Price</option>
+                                        <option value="">Giá từ:</option>
                                         <option value="">$100</option>
                                         <option value="">$200</option>
                                         <option value="">$300</option>
@@ -68,7 +66,30 @@
                                 </div>
                                 <div class="col-md-6">
                                     <select name="" id="" class="form-control w-100">
-                                        <option value="">Max Price</option>
+                                        <option value="">Đến Giá:</option>
+                                        <option value="">$25,000</option>
+                                        <option value="">$50,000</option>
+                                        <option value="">$75,000</option>
+                                        <option value="">$100,000</option>
+                                        <option value="">$100,000,000</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 form-group">
+                            <div class="row">
+                                <div class="col-md-6 form-group">
+                                    <select name="" id="" class="form-control w-100">
+                                        <option value="">Từ ngày:</option>
+                                        <option value="">$100</option>
+                                        <option value="">$200</option>
+                                        <option value="">$300</option>
+                                        <option value="">$400</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <select name="" id="" class="form-control w-100">
+                                        <option value="">Đến Ngày:</option>
                                         <option value="">$25,000</option>
                                         <option value="">$50,000</option>
                                         <option value="">$75,000</option>
@@ -79,36 +100,21 @@
                             </div>
                         </div>
                     </div>
+                    <!--nút submit-->
                     <div class="row">
                         <div class="col-md-4">
                             <input type="submit" class="btn btn-black py-3 btn-block" value="Submit">
                         </div>
                     </div>
-
                 </div>
-                <div class="tab-pane" id="for-sale" role="tabpanel" aria-labelledby="sale-tab">
-                    <div class="row">
-                        <div class="col-md-4 form-group">
-                            <select name="" id="" class="form-control w-100">
-                                <option value="">All Types</option>
-                                <option value="">Townhouses</option>
-                                <option value="">Duplexes</option>
-                                <option value="">Quadplexes</option>
-                                <option value="">Condominiums</option>
-                            </select>
-                        </div>
-                        <div class="col-md-4 form-group">
-                            <input type="text" class="form-control" placeholder="Title">
-                        </div>
-                        <div class="col-md-4 form-group">
-                            <input type="text" class="form-control" placeholder="Address">
-                        </div>
-                    </div>
 
+                <!--for-sale-->
+                <div class="tab-pane" id="for-sale" role="tabpanel" aria-labelledby="sale-tab">
+                    <!--dòng 1-->
                     <div class="row">
                         <div class="col-md-4 form-group">
                             <select name="" id="" class="form-control w-100">
-                                <option value="">Any Bedrooms</option>
+                                <option value="">Any Bedrooms - phòng ngủ</option>
                                 <option value="">0</option>
                                 <option value="">1</option>
                                 <option value="">2</option>
@@ -116,8 +122,23 @@
                             </select>
                         </div>
                         <div class="col-md-4 form-group">
+                            <input type="text" class="form-control" placeholder="Địa chỉ">
+                        </div>
+                        <div class="col-md-4 form-group">
                             <select name="" id="" class="form-control w-100">
-                                <option value="">Any Bathrooms</option>
+                                <option value="">Thành phố</option>
+                                <option value="">0</option>
+                                <option value="">1</option>
+                                <option value="">2</option>
+                                <option value="">3</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!--dòng 2-->
+                    <div class="row">
+                        <div class="col-md-4 form-group">
+                            <select name="" id="" class="form-control w-100">
+                                <option value="">Any Bathrooms - phòng tắm</option>
                                 <option value="">0</option>
                                 <option value="">1</option>
                                 <option value="">2</option>
@@ -128,7 +149,7 @@
                             <div class="row">
                                 <div class="col-md-6 form-group">
                                     <select name="" id="" class="form-control w-100">
-                                        <option value="">Min Price</option>
+                                        <option value="">Giá từ:</option>
                                         <option value="">$100</option>
                                         <option value="">$200</option>
                                         <option value="">$300</option>
@@ -137,7 +158,30 @@
                                 </div>
                                 <div class="col-md-6">
                                     <select name="" id="" class="form-control w-100">
-                                        <option value="">Max Price</option>
+                                        <option value="">Đến Giá:</option>
+                                        <option value="">$25,000</option>
+                                        <option value="">$50,000</option>
+                                        <option value="">$75,000</option>
+                                        <option value="">$100,000</option>
+                                        <option value="">$100,000,000</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 form-group">
+                            <div class="row">
+                                <div class="col-md-6 form-group">
+                                    <select name="" id="" class="form-control w-100">
+                                        <option value="">Từ ngày:</option>
+                                        <option value="">$100</option>
+                                        <option value="">$200</option>
+                                        <option value="">$300</option>
+                                        <option value="">$400</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <select name="" id="" class="form-control w-100">
+                                        <option value="">Đến Ngày:</option>
                                         <option value="">$25,000</option>
                                         <option value="">$50,000</option>
                                         <option value="">$75,000</option>
@@ -148,12 +192,12 @@
                             </div>
                         </div>
                     </div>
+                    <!--nút submit-->
                     <div class="row">
                         <div class="col-md-4">
                             <input type="submit" class="btn btn-black py-3 btn-block" value="Submit">
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
