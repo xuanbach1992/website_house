67d42768e42f49312f0f6a4c14a3a69585d140b6
TranManhHiep <hieptm.it@gmail.com>
2019-12-14 10:42:12 +0700

    create db seeder district

---
+++ app/District.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class District extends Model
+{
+    protected $table = 'district';
+
+    protected $fillable = [
+        'id', 'name', 'cities_id',
+    ];
+}
---
+++ database/seeds/DistrictTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\District;
+
+class DistrictTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
