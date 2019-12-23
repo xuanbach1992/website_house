2406bc3497a02e51a9f0334ee2ef411449423c00
xuanbach1992 <Xuanbach1992>
2019-12-23 16:51:51 +0700

    fix ratting house

---
+++ database/seeds/StarsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StarsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $star = new \App\Star();
+        $star->id = 1;
+        $star->house_id = 1;
+        $star->number = 4;
+        $star->user_id = 2;
+        $star->content = 'dep nhung giao dien web xau';
+        $star->save();
+    }
+}
